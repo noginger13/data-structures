92dc6e38c2e51fa304a13d5655db114d8c8087fb
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-06-03 14:31:23 -0400

    pass tests for addChild in tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,18 +2,30 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
 
+  // your code here
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = new Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  //Check for value
+  if (this.value === target) {
+    return true;
+  } else if (this.hasChildren) {
+    for (var i = 0; i < this.children.length; i++) {
+      this.chidren[i].contains(target);
+    }
+  }
+  return false;
 };
 
 
