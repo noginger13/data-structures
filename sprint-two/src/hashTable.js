239a6436c15fbc4b55e0b289faa74d1ec839f66f
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-06-05 15:30:56 -0400

    preliminary implementation of insert and retrieve for hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,21 +1,30 @@
 
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set.call(LimitedArray, index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get.call(LimitedArray, index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set.call(LimitedArray, index, undefined);
 };
+
+// limitedArray.each = function(callback) {
+//   for (var i = 0; i < storage.length; i++) {
+//     callback(storage[i], i, storage);
+//   }
+// };
 
 
 
